bf3bb57012bf74f7465bec41a9bcb84c94e14bb2
luojia65 <me@luojia.cc>
2021-04-27 02:32:21 +0800

    Small fix

--- src/hal/mod.rs
+++ src/hal/mod.rs
@@ -2,29 +2,29 @@ pub mod digital;
 
 use digital::*;
 
-pub type MyGpioPin<MODE, const N: usize> = GpioPin<MODE, 0x1060_0000, N>;
+pub type MyGpioPeripheral = GpioPeripheral<0x1060_0000>;
 
-pub struct MyGpioPeripheral {
-    pub pin0: MyGpioPin<Input, 0>,
-    pub pin1: MyGpioPin<Input, 1>,
-    pub pin2: MyGpioPin<Input, 2>,
-    pub pin3: MyGpioPin<Input, 3>,
-    pub pin4: MyGpioPin<Input, 4>,
-    pub pin5: MyGpioPin<Input, 5>,
-    pub pin6: MyGpioPin<Input, 6>,
-    pub pin7: MyGpioPin<Input, 7>,
-    pub pin8: MyGpioPin<Input, 8>,
-    pub pin9: MyGpioPin<Input, 9>,
-    pub pin10: MyGpioPin<Input, 10>,
-    pub pin11: MyGpioPin<Input, 11>,
-    pub pin12: MyGpioPin<Input, 12>,
-    pub pin13: MyGpioPin<Input, 13>,
-    pub pin14: MyGpioPin<Input, 14>,
-    pub pin15: MyGpioPin<Input, 15>,
+pub struct GpioPeripheral<const P: usize> {
+    pub pin0: GpioPin<Input, P, 0>,
+    pub pin1: GpioPin<Input, P, 1>,
+    pub pin2: GpioPin<Input, P, 2>,
+    pub pin3: GpioPin<Input, P, 3>,
+    pub pin4: GpioPin<Input, P, 4>,
+    pub pin5: GpioPin<Input, P, 5>,
+    pub pin6: GpioPin<Input, P, 6>,
+    pub pin7: GpioPin<Input, P, 7>,
+    pub pin8: GpioPin<Input, P, 8>,
+    pub pin9: GpioPin<Input, P, 9>,
+    pub pin10: GpioPin<Input, P, 10>,
+    pub pin11: GpioPin<Input, P, 11>,
+    pub pin12: GpioPin<Input, P, 12>,
+    pub pin13: GpioPin<Input, P, 13>,
+    pub pin14: GpioPin<Input, P, 14>,
+    pub pin15: GpioPin<Input, P, 15>,
 }
 
-impl MyGpioPeripheral {
-    pub fn take() -> Option<MyGpioPeripheral> {
+impl<const P: usize> GpioPeripheral<P> {
+    pub fn take() -> Option<GpioPeripheral<P>> {
         // note(unsafe): 获得所有权
         Some(unsafe { core::mem::transmute(()) })
     }
